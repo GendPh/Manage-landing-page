903c1fe22a5c4cbf2d5b41bd20c089fe106ac25f
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-06-22 12:03:43 +0100

    chore: Update cSpell dictionary and import dependencies
    ```

--- manage-landing-page/src/app/footer/footer.component.ts
+++ manage-landing-page/src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './footer.component.html',
   styles: ``
 })
